225837bd9b69f8dfaee428b0182b5d4c27d3be30
Vishal Vipinmohan <47913510+vishalvipin1@users.noreply.github.com>
2024-04-13 18:17:37 +0530

    Update client.py

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ try:
         data=client.recv(1024)
         if not data:
             break
-        print(f"Message: {data.decode("utf-8")}")
+        print(f"Message: {data.decode('utf-8')}")
 except OSError as e:
     pass
 
