aee9899aad3fe664f3c77d86a9390d8868a6695a
Vishal Vipinmohan <47913510+vishalvipin1@users.noreply.github.com>
2024-04-13 18:05:06 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,18 +1,18 @@
 import socket
 
-client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-client.connect(("0C-DD-24-18-BE-9B"))
+client=socket.socket(socket.AF_BLUETOOTH,socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+client.connect(("0C-DD-24-18-BE-9B",4))
+
 
 try:
     while True:
-        message = input("Enter messge: ")
-        client.send(message.encode('utf-8'))
-        data = client.recv(1024)
+        message=input("Enter message: ")
+        client.send(message.encode("utf-8"))
+        data=client.recv(1024)
         if not data:
             break
-        print(f"Message: {data.decode('utf-8')}")
+        print(f"Message: {data.decode("utf-8")}")
 except OSError as e:
     pass
 
 client.close()
-
--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 import socket
 
-server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-server.bind(("0C-DD-24-18-BE-9B"))
+
+server=socket.socket(socket.AF_BLUETOOTH,socket.SOCK_STREAM,socket.BTPROTO_RFCOMM)
+server.bind(("0C-DD-24-18-BE-9B",4))
 server.listen(1)
 
-client, addr = server.accept()
+
+client,addr=server.accept()
 
 try:
     while True:
-        data = client.recv(1024)
+        data=client.recv(1024)
         if not data:
             break
         print(f"Message: {data.decode('utf-8')}")
-        message = input("Enter message:")
+        message=input("Enter message: ")
         client.send(message.encode("utf-8"))
+        
 except OSError as e:
     pass
 
