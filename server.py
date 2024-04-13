7068ad561b62eb9e82bc5bc13845c4da3ad053c6
Vishal Vipinmohan <vishalvipin123@gmail.com>
2024-04-13 16:58:23 +0530

    Hello

---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+
+server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+server.bind(("0C-DD-24-18-BE-9B"))
+server.listen(1)
+
+client, addr = server.accept()
+
+try:
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Message: {data.decode('utf-8')}")
+        message = input("Enter message:")
+        client.send(message.encode("utf-8"))
+except OSError as e:
+    pass
+
+client.close()
+server.close()
